b5d4a42fef91bb6425f6158395e91b7b3950f62a
Witold Orzeszko <orzeszkowitold@gmail.com>
2019-06-30 17:42:54 +0200

    first model created and second migration done

--- Django/twitter/models.py
+++ Django/twitter/models.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+class Tweet(models.Model):
+    content = models.CharField(max_length=32)
+    date = models.DateTimeField(default=datetime.now)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
